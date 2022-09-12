0143f6896990d66593d53bf0c28ff445d70157de
Joon Park <jsp220@gmail.com>
2022-09-12 09:16:53 -0700

    added today's weather icon and temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
 var cardTitles = [$(".card-title-1"), $(".card-title-2"), $(".card-title-3"), $(".card-title-4"), $(".card-title-5")];
-var todayEl = $(".today");
+var cityEl = $("#city");
+var todayEl = $("#today");
+var todayImgEl = $("#today-icon");
+var todayTempEl = $("#today-temp")
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=San%20Diego&units=imperial&APPID=3156f6ab42af6efa3211c272e0ef77df";
 
 todayEl.text(moment().format("(MM/DD/YYYY)"));
 for (var i in cardTitles) {
     cardTitles[i].text(moment().add(1, "day").add(i, "day").format("MM/DD/YYYY"));
 }
+
+fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var icon = data.weather[0].icon;
+        console.log(data);
+        
+        var imgUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
+        todayImgEl.attr("src", imgUrl);
+        todayTempEl.text(data.main.temp);
+
+    })
