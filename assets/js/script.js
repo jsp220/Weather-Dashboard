cdd8dbaeb1ac8f4aab0f15bf9980ba3baaa1ff9a
Joon Park <jsp220@gmail.com>
2022-09-11 17:08:55 -0700

    dates populated with moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello")
+var cardTitles = [$(".card-title-1"), $(".card-title-2"), $(".card-title-3"), $(".card-title-4"), $(".card-title-5")];
+var todayEl = $(".today");
+
+todayEl.text(moment().format("(MM/DD/YYYY)"));
+for (var i in cardTitles) {
+    cardTitles[i].text(moment().add(1, "day").add(i, "day").format("MM/DD/YYYY"));
+}
